ee624d366508897662a86f8ccb0f7750b53a479c
Benk Home <mail.buat.forum@gmail.com>
2021-06-23 00:33:50 +0700

    before Breaking Free of Network Dependency

--- app/src/main/java/com/raywenderlich/android/redditclone/repositories/RedditPagingSource.kt
+++ app/src/main/java/com/raywenderlich/android/redditclone/repositories/RedditPagingSource.kt
@@ -63,5 +63,5 @@ class RedditPagingSource(private val redditService: RedditService) :
         }
     }
 
-    override val keyReuseSupported: Boolean = false
+    override val keyReuseSupported: Boolean = true
 }
--- app/src/main/java/com/raywenderlich/android/redditclone/ui/RedditLoadingAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/redditclone/ui/RedditLoadingAdapter.kt
@@ -37,21 +37,46 @@ package com.raywenderlich.android.redditclone.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.paging.LoadState
 import androidx.paging.LoadStateAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.android.redditclone.R
+import kotlinx.android.synthetic.main.item_loading_state.view.*
 
 class RedditLoadingAdapter(private val retry: () -> Unit) :
     LoadStateAdapter<RedditLoadingAdapter.LoadingStateViewHolder>() {
 
+
+
     class LoadingStateViewHolder(itemView: View, retry: () -> Unit) :
         RecyclerView.ViewHolder(itemView) {
-        //TODO: not implemented
+
+        private val tvErrorMessage: TextView = itemView.tvErrorMessage
+        private val progressBar: ProgressBar = itemView.progress_bar
+        private val btnRetry: Button = itemView.btnRetry
+
+        init {
+            btnRetry.setOnClickListener {
+                retry()
+            }
+        }
+        
+        fun bindState(loadState: LoadState) {
+            if (loadState is LoadState.Error) {
+                tvErrorMessage.text = loadState.error.localizedMessage
+            }
+            progressBar.isVisible = loadState is LoadState.Loading
+            tvErrorMessage.isVisible = loadState !is LoadState.Loading
+            btnRetry.isVisible = loadState !is LoadState.Loading
+        }
     }
 
     override fun onBindViewHolder(holder: LoadingStateViewHolder, loadState: LoadState) {
-        TODO("not implemented")
+        holder.bindState(loadState)
     }
 
     override fun onCreateViewHolder(
